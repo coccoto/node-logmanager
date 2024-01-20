3f0903293710e88b91c7a7de0390c3abf949a653
coccoto <soprano.lune@gmail.com>
2024-01-20 22:17:18 +0900

    2024-01-20 22:17:18

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-const LoggerManager = require('LoggerManager')
+// const LoggerManager = require('LoggerManager')
 
+const LoggerManager = () => {
+    return 'test'
+}
 export default LoggerManager
