4d88c88e6843229e94073a56cd8e8c6094c55772
coccoto <soprano.lune@gmail.com>
2025-01-25 16:27:05 +0900

    2025-01-25 16:27:05

--- src/LogManager.ts
+++ src/LogManager.ts
@@ -1,28 +1,26 @@
-import winston, { Logger, LogEntry } from 'winston'
+import winston, { Logger } from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const transports = (): winston.transport[] => {
-    switch (process.env['ENVIRONMENT']) {
-        case 'development':
-            return [
-                new winston.transports.Console(),
-                new DailyRotateFile({
-                    filename: process.env['LOG_DIR'] + '/%DATE%.log',
-                    datePattern: 'YYYY',
-                }),
-            ]
-        case 'production':
-            return [
-                new DailyRotateFile({
-                    filename: process.env['LOG_DIR'] + '/%DATE%.log',
-                    datePattern: 'YYYY',
-                }),
-            ]
-        default:
-            throw new Error("[ERROR] Invalid environment. env['ENVIRONMENT']: " + process.env['ENVIRONMENT'])
+    const dailyRotateFile = new DailyRotateFile({
+        filename: process.env['LOG_DIR'] + '/%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        maxFiles: '180',
+    })
+    if (process.env['ENVIRONMENT'] === 'development') {
+        return [
+            new winston.transports.Console(),
+            dailyRotateFile,
+        ]
+    } else if (process.env['ENVIRONMENT'] === 'production') {
+        return [
+            dailyRotateFile,
+        ]
+    } else {
+        throw new Error("[ERROR] Invalid environment. env['ENVIRONMENT']: " + process.env['ENVIRONMENT'])
     }
 }
 
@@ -30,9 +28,7 @@ const logManager = (): Logger => winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-        winston.format.printf((info: LogEntry) => {
-            return JSON.stringify(info)
-        }),
+        winston.format.json(),
     ),
     transports: transports()
 })
